e6fb63b9c67a92a204fed44eb396330f62059e50
AllMighty-57 <ecainepetty@gmail.com>
2026-02-26 11:51:46 -0700

    Animated UI + end goal

--- Cubamunga/Assets/Scripts/GameManager.cs
+++ Cubamunga/Assets/Scripts/GameManager.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 { 
     bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
     public void endGame()
     {
-        Debug.Log("Game Over");
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        } 
+    } 
+
+    void Restart()
+    {
+        SceneManager.LoadScene("Level01");
     }
 }
---
+++ Cubamunga/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LevelComplete : MonoBehaviour
+{
+    public GameManager gameManager;
+    private void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel();
+    }
+}
