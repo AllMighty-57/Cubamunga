9f3c068b9e39bb514d3487a23f67dd3221186c70
AllMighty-57 <ecainepetty@gmail.com>
2026-03-11 11:45:58 -0700

    Finish Level 4

--- Cubamunga/Assets/Scripts/PlayerBehavior.cs
+++ Cubamunga/Assets/Scripts/PlayerBehavior.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public Rigidbody rb;
+
+    public TMP_Text changingColorText;
+    public float redVal; 
+    public float greenVal;
+    public float blueVal;
 
     public float forwardForce = 2000f;
 
@@ -38,6 +45,7 @@ public class PlayerBehavior : MonoBehaviour
             Debug.Log("Object hit");
             forwardForce = 0f;
             sidewaysForce = 0f;
+            changingColorText.color = new Color( redVal/ 255f, greenVal/ 255f, blueVal/ 255f, 1f);
 
             FindAnyObjectByType<GameManager>().endGame();
         }
